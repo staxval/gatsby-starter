a9a8f1493f55f128e9816fc26800cea5856a854d
Vitaliy Starov <staxval@gmail.com>
2019-08-02 01:57:11 +0300

    add mdx page

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,13 @@ module.exports = {
   plugins: [
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-emotion',
-  ]
-}
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/Layout.jsx'),
+        },
+      },
+    },
+  ],
+};
